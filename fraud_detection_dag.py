e253e970a438dff92508e61f662aacbd7a94f226
Ronit Hara Yandra <yandra.r@northeastern.edu>
2024-09-26 23:21:31 -0400

    some changes to dag file

--- fraud_detection_dag.py
+++ fraud_detection_dag.py
@@ -1,6 +1,7 @@
 import os
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+import numpy as np
 from datetime import datetime
 from modeling import FraudDetection
 from sklearn.metrics import classification_report
@@ -13,47 +14,53 @@ def check_file_path(file_path: str):
         raise FileNotFoundError(f"Error: The file {file_path} does not exist.")
     print(f"File {file_path} found.")
 
-def load_data(**kwargs):
-    fraud_detector = kwargs['ti'].xcom_pull(task_ids ='create_fraud_detector')
-    X, y = fraud_detector.load_data()
-    return X, y
-
 def create_fraud_detector(**kwargs):
+    print("Creating fraud detection instance.....")
+    #initialize the FraudDetection object again in the new task
     fraud_detector = FraudDetection(file_path)
+    print("FraudDetection instance has been succesfully created.")
     #push the object to Xcom to use in other tasks
-    kwargs['ti'].xcom_push(key = 'fraud_detector', value = fraud_detector)
-
-def preprocess_data(**kwargs):
-    fraud_detector = kwargs['ti'].xcom_pull(task_ids = 'create_fraud_detector')
+    kwargs['ti'].xcom_push(key = 'status', value = 'fraud_detector_created')
+    #save the preprocessed to a temporary file for reuse
     X, y = fraud_detector.load_data()
     X_scaled = fraud_detector.preprocess(X)
-    return X_scaled, y
+    np.savez('/tmp/preprocessed_data.npz', X_scaled = X_scaled, y = y) 
 
 def undersampling_data(**kwargs):
-    fraud_detector = kwargs['ti'].xcom_pull(task_ids = 'create_fraud_detector')
-    X_scaled, y = kwargs['ti'].xcom_pull(task_ids = 'preprocess_data')
+    fraud_detector = FraudDetection(file_path)
+    #load the preprocessed data from the saved temporary file
+    data = np.load('tmp/preprocessed_data.npz')
+    X_scaled, y = data['X_sacled'], data['y']
     X_res, y_res = fraud_detector.undersampling(X_scaled, y)
-    return X_res, y_res
+    #save undersampled data to a temporary file
+    np.savez('/tmp/undersampled_data.npz', X_res = X_res, y_res = y_res)
 
 def split_data(**kwargs):
-    fraud_detector = kwargs['ti'].xcom_pull(task_ids = 'create_fraud_detector')
-    X_res, y_res = kwargs['ti'].xcom_pull(task_ids = 'undersampling_data')
+    fraud_detector = FraudDetection(file_path)
+    #load the undersampled data
+    data = np.load('/tmp/undersampled_data.npz')
+    X_res, y_res = data['X_res'], data['y_res']
     X_train, X_test, y_train, y_test = fraud_detector.traintest_split(X_res, y_res)
-    return X_train, X_test, y_train, y_test
+    #save the split data to the temporary file
+    np.savez('/tmp/split_data.npz', X_train = X_train, X_test = X_test, y_train = y_train, y_test = y_test)
 
 def isolation_forest(**kwargs):
-    fraud_detector = kwargs['ti'].xcom_pull(task_ids = 'create_fraud_detector')
-    X_train, X_test, y_train, y_test = kwargs['ti'].xcom_pull(task_ids = 'split_data')
+    fraud_detector = FraudDetection(file_path)
+    #load the split data
+    data = np.load('/tmp/split_data.npz')
+    X_train, X_test, y_train, y_test = data['X_train'], data['X_test'], data['y_train'], data['y_test']
     isolation = fraud_detector.isolationForest(X_train)
     y_pred = fraud_detector.predict(isolation, X_test)
     auc = fraud_detector.evaluate(y_test, y_pred)
     print(f"Isolation forest AUPRC(Area Under Precision-Recall Curve): {auc:.4f}")
     print("\nIsolation forest classification report: ")
-    print(classification_report(y_test, y_pred, zero_division = 1))
+    print(classification_report(np.array(y_test), y_pred, zero_division = 1))
 
 def train_autoencoder(**kwargs):
-    fraud_detector = kwargs['ti'].xcom_pull(task_ids = 'create_fraud_detector')
-    X_train, X_test, y_train, y_test = kwargs['ti'].xcom_pull(task_ids = 'split_data')
+    fraud_detector = FraudDetection(file_path)
+    #load the split data again in here
+    data = np.load('/tmp/split_data.npz')
+    X_train, X_test, y_train, y_test = data['X_train'], data['X_test'], data['y_train'], data['y_test']
     autoencoder = fraud_detector.train_encoder(X_train)
     threshold = fraud_detector.threshold_selection(autoencoder, X_test, y_test)
     print(f"selected balanced threshold with better recall rate: {threshold:.4f}")
@@ -78,39 +85,28 @@ with DAG('fraud_detection_pipeline', default_args = default_args, schedule_inter
 
     create_fraud_detector_task = PythonOperator(
         task_id = 'create_fraud_detector',
-        python_callable = create_fraud_detector,
-        provide_context = True
-    )
-
-    preprocess_task = PythonOperator(
-        task_id = 'preprocess_data',
-        python_callable = preprocess_data,
-        provide_context = True
+        python_callable = create_fraud_detector
     )
 
     undersample_task = PythonOperator(
         task_id = 'undersampling_data',
-        python_callable = undersampling_data,
-        provide_context = True
+        python_callable = undersampling_data
     )
 
     split_data_task = PythonOperator(
         task_id = 'split_data',
-        python_callable = split_data,
-        provide_context = True
+        python_callable = split_data
     )
 
     isolation_forest_task = PythonOperator(
         task_id = 'isolation_forest',
-        python_callable = isolation_forest,
-        provide_context = True
+        python_callable = isolation_forest
     )
 
     train_autoencoder_task = PythonOperator(
         task_id = 'train_autoencoder',
-        python_callable = train_autoencoder,
-        provide_context = True
+        python_callable = train_autoencoder
     )
 
     #Defining the task dependencies
-    check_file_task >> create_fraud_detector_task >> preprocess_task >> undersample_task >> split_data_task >> isolation_forest_task >> train_autoencoder_task
+    check_file_task >> create_fraud_detector_task >> undersample_task >> split_data_task >> isolation_forest_task >> train_autoencoder_task
